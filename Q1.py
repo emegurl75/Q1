4f65dc35b01fe4c0ac6e10953e7cd4d71ddbc0c5
Emelyn Jane Mantilla <mantilaemelyn65@gmail.com>
2023-10-12 05:49:17 -0700

    First Commit

---
+++ Q1.py
@@ -0,0 +1,12 @@
+#Value of the price in Liters
+price = 70
+liters = 7
+
+#Calculate the total amount of price
+totalamountofprice = liters * 70
+
+#Display Result
+print("The total amount of price", totalamountofprice)
+
+
+  
